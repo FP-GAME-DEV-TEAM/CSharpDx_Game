b5691fd9fc873c918fc9b40ceb53dfcde3b5c5f2
ElementCraft <976228284@qq.com>
2016-02-01 17:23:19 +0800

    Add Docs
    Docs

--- DXDriver/MainScene.cs
+++ DXDriver/MainScene.cs
@@ -14,14 +14,19 @@ namespace DXDriver
 
 		public override bool Init()
 		{
+			//新的层
 			Layer l = new Layer();
+			//新建一个物件 Widget是继承Node类的
 			Widget w = new Widget();
-
+			
+			//测试用
 			w.ani.StartDisplay(true);
 
 			TimerUtils.Start(4000, 1, "s", n => { w.ani.StopDisplay(); });
 			
+			//把物件添加到层
 			l.addNode(w);
+			//层添加到场景
 			addLayer(l);
 			return true;
 		}
